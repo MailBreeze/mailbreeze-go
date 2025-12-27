14340261bb352b4f57930b3de419251ddef60843
umar <mafulun4real@gmail.com>
2025-12-27 04:02:56 +0100

    Bump version to 1.0.3

--- mailbreeze.go
+++ mailbreeze.go
@@ -22,7 +22,7 @@ import (
 )
 
 // Version is the SDK version.
-const Version = "0.1.0"
+const Version = "1.0.3"
 
 // DefaultBaseURL is the default API base URL.
 const DefaultBaseURL = "https://api.mailbreeze.com/api/v1"
